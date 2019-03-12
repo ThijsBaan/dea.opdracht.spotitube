dbbda69ce73c982c717193ced1fd8d3c8a2a112f
Thijs <tmbaan@gmail.com>
2019-03-12 16:30:24 +0100

    Tracks worden nu juist weergegeven per playlist. Totale afspeelduur wordt nu ook correct weergegeven.

--- src/main/java/nl/thijs/dea/controllers/PlaylistController.java
+++ src/main/java/nl/thijs/dea/controllers/PlaylistController.java
@@ -1,52 +1,89 @@
 package nl.thijs.dea.controllers;
 
-import nl.thijs.dea.dto.LoginResponseDto;
-import nl.thijs.dea.dto.PlaylistRequestDto;
+import nl.thijs.dea.dto.PlaylistRepsonseDto;
+import nl.thijs.dea.dto.TrackResponseDto;
 import nl.thijs.dea.dummy.DummyPlaylists;
+import nl.thijs.dea.dummy.DummyTracks;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 /**
  * PlaylistController --- class for letting user view the playlists
+ *
  * @author Thijs
  */
 @Path("/")
+@Produces("application/json")
 public class PlaylistController {
-    private List<DummyPlaylists> musicList = new ArrayList<>();
-    private String token;
+    private List<DummyPlaylists> musicPlaylist = new ArrayList<>();
+    private List<DummyTracks> trackList = new ArrayList<>();
+    private int playlistLength = 0;
 
     /**
      * Constructor:
-     * add two DummyPlaylists for PlaylistController and add them to the ArrayList musicList.
+     * add two DummyPlaylists for PlaylistController and add them to the ArrayList musicPlaylist.
      */
     public PlaylistController() {
-        musicList.add(new DummyPlaylists(1, "Death metal", true, ""));
-        musicList.add(new DummyPlaylists(2, "Pop", false, ""));
-        musicList.add(new DummyPlaylists(3, "Disney", true, ""));
+        trackList.add(new DummyTracks(3, "Ocean and a rock", "Lisa Hannigan", 337, "Sea sew", false));
+        trackList.add(new DummyTracks(4, "So Long, Marianne", "Leonard Cohen", 546, "Songs of Leonard Cohen", false));
+        trackList.add(new DummyTracks(5, "One", "Metallica", 423, "", 37, "1-11-2001", "Long version", false));
+
+        int[] tracks1 = {4, 3};
+        int[] tracks2 = {5};
+        int[] tracks3 = {5, 4, 3};
+
+
+        musicPlaylist.add(new DummyPlaylists(1, "Death metal", true, tracks1));
+        musicPlaylist.add(new DummyPlaylists(2, "Pop", false, tracks2));
+        musicPlaylist.add(new DummyPlaylists(3, "Disney", true, tracks3));
     }
 
     @GET
     @Path("playlists")
     @Produces("application/json")
     public Response geefResponse(@QueryParam("token") String token) {
-        if ("".equals(token)) {
-            // 400: Bad Request. Something is wrong with the request.
-            // This could be due to a missing query-parameter for the token.
-            return Response.status(400).build();
-        } else if ("ABCDEFG".equals(token)) { //"ABCDEFG" vervangen voor opgeslagen code uit database
-            PlaylistRequestDto response = new PlaylistRequestDto();
+        if ("ABCDEFG".equals(token)) { //"ABCDEFG" vervangen voor opgeslagen code uit database
+            PlaylistRepsonseDto response = new PlaylistRepsonseDto();
 
-            response.setPlaylists(musicList);
-            response.setLength(37);
+            for (DummyTracks s: trackList) {
+                playlistLength += s.getDuration();
+            }
+
+            response.setPlaylists(musicPlaylist);
+            response.setLength(playlistLength);
 
             return Response.ok().entity(response).build();
+        } else if ("".equals(token)) {
+            return Response.status(400).build();
         } else {
-            // 403: Forbidden. The request was valid, but you have requested a resource for which are not authorized.
-            // This will probably mean you have provided a token that is invalid.
             return Response.status(403).build();
         }
+    }
+
+    @GET
+    @Path("playlists/{forplaylist}/tracks")
+    public Response geefTracks(@PathParam("forplaylist") int forPlayList, @QueryParam("token") String token) {
+        List<DummyTracks> tempTrackList = new ArrayList<>();
+
+        for (DummyPlaylists m : musicPlaylist) {
+            if (m.getId() == forPlayList) {
+                for (DummyTracks t : trackList) {
+                    for (int i : m.getTracks()) {
+                        if (i == t.getId()) {
+                            tempTrackList.add(t);
+                        }
+                    }
+                }
+
+                var response = new TrackResponseDto();
+                response.setTracks(tempTrackList);
+                return Response.ok().entity(response).build();
+            }
+        }
+        return null;
     }
 }
