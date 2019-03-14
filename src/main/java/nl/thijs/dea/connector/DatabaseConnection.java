67a237dd48b704ced2c39dff130ba4fdf95831d5
Thijs <tmbaan@gmail.com>
2019-03-14 11:19:40 +0100

    Database connection aangemaakt.

---
+++ src/main/java/nl/thijs/dea/connector/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package nl.thijs.dea.connector;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+    private Connection connection = null;
+
+    public DatabaseConnection() {
+        connect();
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    private void connect() {
+        try {
+            String userID = "sa";
+            String password = "password123";
+
+            String connectionUrl = "jdbc:sqlserver://localhost;databaseName=dea_spotitube";
+
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            connection = DriverManager.getConnection(connectionUrl, userID, password);
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("An error message has occurred: " + e);
+        }
+    }
+}
