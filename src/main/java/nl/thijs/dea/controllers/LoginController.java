cfa4d53d1974194cfe546e970f19199ba66ef01f
Thijs <tmbaan@gmail.com>
2019-03-14 11:20:36 +0100

    Login gemaakt op de Users uit de database in plaats van hard-coded users.

--- src/main/java/nl/thijs/dea/controllers/LoginController.java
+++ src/main/java/nl/thijs/dea/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package nl.thijs.dea.controllers;
 
+import nl.thijs.dea.connector.DatabaseConnection;
 import nl.thijs.dea.dto.LoginRequestDto;
 import nl.thijs.dea.dto.LoginResponseDto;
 import nl.thijs.dea.dummy.DummyUsers;
@@ -8,58 +9,70 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 /**
  * LoginController --- class for letting user log in to the application
+ *
  * @author Thijs
  */
 @Path("/")
 @Produces("application/json")
 public class LoginController {
     private List<DummyUsers> users = new ArrayList<>();
+    private DatabaseConnection conc;
 
     /**
      * Constructor:
-     * add two DummyUsers for LoginController and add them to the ArrayList users.
+     * Make a new object for the database connection.
      */
     public LoginController() {
-        users.add(new DummyUsers("thijs", "baan"));
-        users.add(new DummyUsers("piet", "jansen"));
+        conc = new DatabaseConnection();
     }
 
     /**
      * Let the user login if the user is authorized.
+     *
      * @param request an LoginRequestDto object that contains
      *                an Username and Password.
      * @return response with token and username
      */
     @POST
     @Path("login")
-    public Response login(LoginRequestDto request){
+    public Response login(LoginRequestDto request) throws SQLException {
+        var connection = conc.getConnection();
+        PreparedStatement st = connection.prepareStatement("SELECT Username, Password FROM Login WHERE Username = ? " +
+                "AND Password = ?");
+        st.setString(1, request.getUser());
+        st.setString(2, request.getPassword());
 
-        for (DummyUsers u : users) {
-            if (u.getUsername().equals(request.getUser()) && u.getPassword().equals(request.getPassword())){
-                LoginResponseDto response = new LoginResponseDto();
+        ResultSet resultSet = st.executeQuery();
 
-                String token = randomTokenGenerator();
-                String user = u.getFullName();
+        if (resultSet.next()) {
+            LoginResponseDto response = new LoginResponseDto();
 
-                response.setToken(token);
-                response.setUser(user);
+            var u = new DummyUsers(request.getUser(), request.getPassword());
 
-                return Response.ok().entity(response).build();
-            }
+            String token = randomTokenGenerator();
+            String user = u.getFullName();
+
+            response.setToken(token);
+            response.setUser(user);
+
+            return Response.ok().entity(response).build();
         }
-
         return Response.status(401).build();
     }
 
     /**
      * Generates a string with the length of "tokenLength"
      * and containts the SALTCHARS-characters.
+     *
      * @return random generated string with specific length
      */
     private String randomTokenGenerator() {
