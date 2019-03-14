ad58129eddc32f3f5980d97002d466bff82a9717
Thijs <tmbaan@gmail.com>
2019-03-14 12:18:53 +0100

    Launcher

--- src/main/java/nl/thijs/dea/Spotitube.java
+++ src/main/java/nl/thijs/dea/Spotitube.java
@@ -2,12 +2,16 @@ package nl.thijs.dea;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/")
 public class Spotitube {
 
+
     @GET
-    public String hello(){
-        return "Hello World";
+    @Produces("application/json")
+    public Response hello() {
+        return Response.ok().entity("Hello World").build();
     }
 }
