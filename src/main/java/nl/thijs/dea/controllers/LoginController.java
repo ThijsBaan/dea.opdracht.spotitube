e3863e67c69aca5f026d3ffd76c25aca442122be
Thijs <tmbaan@gmail.com>
2019-03-16 22:07:50 +0100

    Login aangepast voor eenmalige token

--- src/main/java/nl/thijs/dea/controllers/LoginController.java
+++ src/main/java/nl/thijs/dea/controllers/LoginController.java
@@ -11,10 +11,6 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.Random;
-
 /**
  * LoginController --- class for letting user log in to the application
  *
@@ -26,7 +22,7 @@ public class LoginController {
     private LoginDAO loginDAO;
 
     @Inject
-    public void setLoginDAO(LoginDAO loginDAO){
+    public void setLoginDAO(LoginDAO loginDAO) {
         this.loginDAO = loginDAO;
     }
 
@@ -44,39 +40,15 @@ public class LoginController {
         UserModel user = loginDAO.login(request.getUser(), request.getPassword());
 
         if ((!user.getUsername().equals("")) || user.getUsername() != null) {
-                LoginResponseDto response = new LoginResponseDto();
+            LoginResponseDto response = new LoginResponseDto();
 
-                // Genereer een random token
-                String token = randomTokenGenerator();
-                // Voeg een token toe aan de database
-                loginDAO.insertTokenWithUser(user.getUsername(), token);
-                response.setToken(token);
-                response.setUser(response.makeFullname(user.getUsername(),user.getPassword()));
+            loginDAO.insertTokenIfUsersFirstTime(user.getUsername());
+            response.setToken(loginDAO.getToken());
+            response.setUser(response.makeFullname(user.getUsername(), user.getPassword()));
 
-                return Response.ok().entity(response).build();
-            }
-
-        return Response.status(401).build();
-    }
-
-    /**
-     * Generates a string with the length of "tokenLength"
-     * and containts the SALTCHARS-characters.
-     *
-     * @return random generated string with specific length
-     */
-    private String randomTokenGenerator() {
-        final int TOKENLENGTH = 15;
-        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-
-        StringBuilder salt = new StringBuilder();
-        Random rnd = new Random();
-
-        while (salt.length() < TOKENLENGTH) { // length of the random string.
-            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
-            salt.append(SALTCHARS.charAt(index));
+            return Response.ok().entity(response).build();
         }
 
-        return salt.toString();
+        return Response.status(401).build();
     }
 }
--- src/main/java/nl/thijs/dea/datasources/doa/LoginDAO.java
+++ src/main/java/nl/thijs/dea/datasources/doa/LoginDAO.java
@@ -8,24 +8,26 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
 
 public class LoginDAO {
     private Connection connection;
+    private String token;
 
     @Inject
     public void setConnection(DatabaseConnection databaseConnection) {
         this.connection = databaseConnection.getConnection();
     }
 
-    public UserModel login(String user, String password){
-        if (checkIfLoginIsCorrect(user, password)){
+    public UserModel login(String user, String password) {
+        if (checkIfLoginIsCorrect(user, password)) {
             return new UserModel(user, password);
         }
         return null;
     }
 
     /**
-     * @param user the username that was used in the userform.
+     * @param user     the username that was used in the userform.
      * @param password the password that was used in the loginform.
      * @return a row executed PreparedStatement for the login with the given params
      */
@@ -47,20 +49,75 @@ public class LoginDAO {
         }
     }
 
-    /**
-     * @param username String that contains the users Username
-     * @param token    String that contains a random token
-     */
-    public void insertTokenWithUser(String username, String token) {
-        try {
-            PreparedStatement tokenSt = connection.prepareStatement("INSERT INTO Token (Username, Token)" +
-                    "VALUES(?, ?)");
-            tokenSt.setString(1, username);
-            tokenSt.setString(2, token);
 
-            tokenSt.executeUpdate();
+    /**
+     * Check if hasntUserGotATokenYet(username) returns false,
+     * when it is false, generate a random Token and insert this into the database
+     *
+     * @param username String that contains the users Username
+     */
+    public void insertTokenIfUsersFirstTime(String username) {
+        try {
+            if (hasntUserGotATokenYet(username)) {
+                token = randomTokenGenerator();
+                PreparedStatement tokenSt = connection.prepareStatement("INSERT INTO Token (Username, Token)" +
+                        "VALUES(?, ?)");
+                tokenSt.setString(1, username);
+                tokenSt.setString(2, token);
+
+                tokenSt.executeUpdate();
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    /**
+     * Do a select in the table Token with the Username.
+     * If there is a result, set the local variable to this result
+     * @param username String that contains the users Username
+     * @return boolean: true if token has been found otherwise false
+     */
+    private boolean hasntUserGotATokenYet(String username) {
+        try {
+            PreparedStatement userTokenSt = connection.prepareStatement("SELECT Token FROM Token WHERE Username = ?");
+            userTokenSt.setString(1, username);
+
+            ResultSet results = userTokenSt.executeQuery();
+
+            if (results.next()) {
+                this.token = results.getString("Token");
+            }
+
+            return "".equals(token);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Generates a string with the length of "tokenLength"
+     * and containts the SALTCHARS-characters.
+     *
+     * @return random generated string with specific length
+     */
+    private String randomTokenGenerator() {
+        final int TOKENLENGTH = 15;
+        String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+        StringBuilder salt = new StringBuilder();
+        Random rnd = new Random();
+
+        while (salt.length() < TOKENLENGTH) { // length of the random string.
+            int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+            salt.append(SALTCHARS.charAt(index));
+        }
+
+        return salt.toString();
     }
 }
